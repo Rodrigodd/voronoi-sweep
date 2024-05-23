c57cbbf3c3810706b9f2f9eb994cfa3444d8e2bb
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2024-05-23 20:08:20 -0300

    Fix circuncenter test

--- src/test.rs
+++ src/test.rs
@@ -657,10 +657,23 @@ fn test_circumcenter(a: (u8, u8), b: (u8, u8), c: (u8, u8)) {
     let cc = circumcenter(p, q, r);
 
     // check if points are collinear
-    {
+    'collinear: {
         let (ax, ay) = (a.0 as i32, a.1 as i32);
         let (bx, by) = (b.0 as i32, b.1 as i32);
         let (cx, cy) = (c.0 as i32, c.1 as i32);
+
+        // `circumcenter` can handle a single duplicated point
+        let mut sites = vec![p, q, r];
+        sites.sort();
+        sites.dedup();
+        if sites.len() == 2 {
+            // unless they are horizontally align
+            if sites[0].pos.y == sites[1].pos.y {
+                assert!(cc.is_none());
+                return;
+            }
+            break 'collinear;
+        }
 
         if (by - ay) * (cx - bx) == (cy - by) * (bx - ax) {
             assert!(cc.is_none());
