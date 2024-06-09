c0186ce0dbb1d5f05fbe42063080616ef4195e8f
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2024-06-09 13:03:51 -0300

    Refactor fmt::Debug to its own file

--- src/btree.rs
+++ src/btree.rs
@@ -1,7 +1,6 @@
 use super::debugln;
 use std::{fmt::Debug, mem::MaybeUninit, ptr::NonNull};
 
-
 macro_rules! test_assert {
     ($cond:expr) => {
         #[cfg(debug_assertions)]
@@ -16,6 +15,8 @@ macro_rules! test_assert {
         }
     };
 }
+
+mod debug;
 
 trait OptionTestExt {
     fn test_assert_none(&self);
@@ -376,20 +377,19 @@ impl<T: Ord + Debug, const N: usize> InternalNode<T, N> {
             return None;
         };
 
-        use std::fmt;
-        pub struct Fmt<F>(pub F)
-        where
-            F: Fn(&mut fmt::Formatter) -> fmt::Result;
-
-        impl<F> fmt::Debug for Fmt<F>
-        where
-            F: Fn(&mut fmt::Formatter) -> fmt::Result,
-        {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                (self.0)(f)
-            }
-        }
-
+        // use std::fmt;
+        // pub struct Fmt<F>(pub F)
+        // where
+        //     F: Fn(&mut fmt::Formatter) -> fmt::Result;
+        //
+        // impl<F> fmt::Debug for Fmt<F>
+        // where
+        //     F: Fn(&mut fmt::Formatter) -> fmt::Result,
+        // {
+        //     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        //         (self.0)(f)
+        //     }
+        // }
         // debugln!(
         //     "splitted~ {:?} {:?} {:?}",
         //     Fmt(|f| self.format(f, depth - 1)),
@@ -552,82 +552,6 @@ impl<T: Ord + Debug, const N: usize> InternalNode<T, N> {
             .all(|x| x.is_some()));
 
         (median, right)
-    }
-}
-impl<T: Debug, const N: usize> Debug for BTree<T, N> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "BTree(")?;
-        write!(f, "depth: {}, ", self.depth)?;
-        if self.depth == 0 {
-            self.root.inner.format(f)?;
-        } else {
-            self.root.format(f, self.depth)?;
-        }
-        write!(f, ")")?;
-        Ok(())
-    }
-}
-
-impl<T: Debug, const N: usize> Debug for InternalNode<T, N> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "~InternalNode(")?;
-        self.inner.format(f)?;
-        write!(f, ")")?;
-        Ok(())
-    }
-}
-
-impl<T: Debug, const N: usize> Debug for LeafNode<T, N> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "LeafNode[")?;
-        for (i, value) in self.values().enumerate() {
-            if i > 0 {
-                write!(f, ", ")?;
-            }
-            write!(f, "{:?}", value)?;
-        }
-        write!(f, "]")
-    }
-}
-
-impl<T: Debug, const N: usize> InternalNode<T, N> {
-    fn format(&self, f: &mut std::fmt::Formatter<'_>, depth: usize) -> std::fmt::Result {
-        test_assert!(depth > 0);
-        write!(f, "[").unwrap();
-        for (child, value) in self.children()[..self.inner.len + 1]
-            .iter()
-            .zip(self.inner.values().map(Some).chain(std::iter::once(None)))
-        {
-            let child = child.expect("child is None");
-            if depth > 1 {
-                let child = unsafe { child.cast::<InternalNode<T, N>>().as_ref() };
-                test_assert!(child.inner.internal, "child is internal");
-                child.format(f, depth - 1)?;
-            } else {
-                let child = unsafe { child.cast::<LeafNode<T, N>>().as_ref() };
-                test_assert!(!child.internal, "child is leaf");
-                child.format(f)?;
-            }
-
-            if let Some(value) = value {
-                write!(f, " {:?} ", value)?;
-            }
-        }
-        write!(f, "]").unwrap();
-        Ok(())
-    }
-}
-
-impl<T: Debug, const N: usize> LeafNode<T, N> {
-    fn format(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[")?;
-        for (i, value) in self.values().enumerate() {
-            if i > 0 {
-                write!(f, ", ")?;
-            }
-            write!(f, "{:?}", value)?;
-        }
-        write!(f, "]")
     }
 }
 
---
+++ src/btree/debug.rs
@@ -0,0 +1,79 @@
+use super::{BTree, InternalNode, LeafNode};
+use std::fmt::Debug;
+
+impl<T: Debug, const N: usize> Debug for BTree<T, N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "BTree(")?;
+        write!(f, "depth: {}, ", self.depth)?;
+        if self.depth == 0 {
+            self.root.inner.format(f)?;
+        } else {
+            self.root.format(f, self.depth)?;
+        }
+        write!(f, ")")?;
+        Ok(())
+    }
+}
+
+impl<T: Debug, const N: usize> Debug for InternalNode<T, N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "~InternalNode(")?;
+        self.inner.format(f)?;
+        write!(f, ")")?;
+        Ok(())
+    }
+}
+
+impl<T: Debug, const N: usize> Debug for LeafNode<T, N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "LeafNode[")?;
+        for (i, value) in self.values().enumerate() {
+            if i > 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{:?}", value)?;
+        }
+        write!(f, "]")
+    }
+}
+
+impl<T: Debug, const N: usize> InternalNode<T, N> {
+    pub(crate) fn format(&self, f: &mut std::fmt::Formatter<'_>, depth: usize) -> std::fmt::Result {
+        test_assert!(depth > 0);
+        write!(f, "[").unwrap();
+        for (child, value) in self.children()[..self.inner.len + 1]
+            .iter()
+            .zip(self.inner.values().map(Some).chain(std::iter::once(None)))
+        {
+            let child = child.expect("child is None");
+            if depth > 1 {
+                let child = unsafe { child.cast::<InternalNode<T, N>>().as_ref() };
+                test_assert!(child.inner.internal, "child is internal");
+                child.format(f, depth - 1)?;
+            } else {
+                let child = unsafe { child.cast::<LeafNode<T, N>>().as_ref() };
+                test_assert!(!child.internal, "child is leaf");
+                child.format(f)?;
+            }
+
+            if let Some(value) = value {
+                write!(f, " {:?} ", value)?;
+            }
+        }
+        write!(f, "]").unwrap();
+        Ok(())
+    }
+}
+
+impl<T: Debug, const N: usize> LeafNode<T, N> {
+    pub(crate) fn format(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[")?;
+        for (i, value) in self.values().enumerate() {
+            if i > 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{:?}", value)?;
+        }
+        write!(f, "]")
+    }
+}
